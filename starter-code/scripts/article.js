4f9caf4e751fe1f5a1610519e19e650905c9b2b7
Brigitte Huneke <brigittehuneke@gmail.com>
2016-12-13 14:09:23 -0800

    did the first three todos

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,16 +1,21 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the object passed in to complete this constructor function:
+  // TODO: DONE Use the object passed in to complete this constructor function:
   // Save ALL the properties of `opts` into `this`.
+  this.title = opts.title;
+  this.category = opts.category;
   this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
 
-  /* TODO: Now use jQuery to fill in the rest of the current
+  /* TODO: DONE Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
     1. author name,
@@ -18,10 +23,12 @@ Article.prototype.toHtml = function() {
     3. article title
     4. article body, and
     5. publication date. */
-
+  $newArticle.find('a').html(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('h1').html(this.title);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
+  $newArticle.find('section.article-body').html(this.body);
   /* TODO: This clone article is no longer a template,
   as it now has real data attached to it! We need to account
   for that before this current article gets rendered to our
